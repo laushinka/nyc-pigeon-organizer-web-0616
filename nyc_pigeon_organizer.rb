34c8129cfb835370437c3ffd170e98045d0c4cbb
laushinka <laurie.malau@gmail.com>
2016-05-18 12:26:30 +0700

    Pass tests

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
-def nyc_pigeon_organizer(data)
-  # write your code here!
+def nyc_pigeon_organizer(pigeon_data)
+    pigeon_list = {}
+
+    pigeon_data.each do |attribute, hash|
+        hash.each do |key, value|
+            value.each do |name|
+                    if !pigeon_list[name]
+                        pigeon_list[name] = {}
+                    end
+                    if !pigeon_list[name][attribute]
+                        pigeon_list[name][attribute] = []
+                    end
+                    pigeon_list[name][attribute].push(key.to_s)
+            end
+        end
+    end
+    pigeon_list
 end
